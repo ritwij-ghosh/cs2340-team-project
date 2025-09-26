71b7f9ae4755e45a71abc352da030d1f4c75c1cb
Ritwij Ghosh <ritwij.ghosh@gmail.com>
2025-09-26 10:53:53 -0400

    user stories 19 and 20 - admin functionalities

--- jobs/views.py
+++ jobs/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Job
 
 def index(request):
+    approved_jobs = Job.objects.filter(status=Job.ModerationStatus.APPROVED).order_by('-created_at')
     context = {
-        'template_data': {'title': 'Jobs - HireBuzz'}
+        'template_data': {'title': 'Jobs - HireBuzz'},
+        'jobs': approved_jobs,
     }
     return render(request, 'jobs/index.html', context)
